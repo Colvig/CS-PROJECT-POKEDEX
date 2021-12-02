b8f7c344395629a7ff0ac5cb463701ada6f550c9
Haley Cahoon <94021394+Colvig@users.noreply.github.com>
2021-12-02 10:21:23 -0500

    Added fonts and size to statistics. Reorganized the order the stats are shown.

--- pokedex.py
+++ pokedex.py
@@ -29,7 +29,7 @@ def main():
 
     # creating a label
     my_label = Label(window, text="Search for a Generation 1 Pokemon",
-    font=("Simsun", 14), fg="black")
+    font=("Simsun", 24), fg="black")
     my_label.pack(pady=20)
 
     # creating an entry box
@@ -37,7 +37,8 @@ def main():
     poke_input.pack(pady=20)               
     
     # creating the search button and adding a binding functon
-    button = Button(window, bd='4', text='Search', fg='black', bg='white', command=lambda: filescanner(pokemon_image, poke_input, ndex, poke_type, poke_hp, poke_atk, poke_def, poke_sp_atk, poke_sp_def, poke_speed))
+    button = Button(window, bd='4', text='Search', fg='black', bg='white', font=("Simsun", 14),
+    command=lambda: filescanner(pokemon_image, poke_input, ndex, poke_type, poke_hp, poke_atk, poke_def, poke_sp_atk, poke_sp_def, poke_speed))
     button.pack()
 
     # creating a list box
@@ -47,38 +48,39 @@ def main():
     # create a label for an image
     pokemon_image = Label(window)
     pokemon_image.pack(side='left', padx=20)
+    
+      # creating a label for the pokemons speed
+    poke_speed = Label(window)
+    poke_speed.pack(side=BOTTOM, pady=10)
 
-    # creating a label for the pokemons pokedex number
-    ndex = Label(window)
-    ndex.pack(side=BOTTOM, pady=10)
-
-    # creating a label for the pokemons type
-    poke_type = Label(window)
-    poke_type.pack(side=BOTTOM, pady=10)
-
-    # creating a label for the pokemons HP
-    poke_hp = Label(window)
-    poke_hp.pack(side=BOTTOM, pady=10)
-
-    # creating a label for the pokemons attack
-    poke_atk = Label(window)
-    poke_atk.pack(side=BOTTOM, pady=10)
-
-    # creating a label for the pokemons defense 
-    poke_def = Label(window)
-    poke_def.pack(side=BOTTOM, pady=10)
+    # creating a label for the pokemons special defense 
+    poke_sp_def = Label(window)
+    poke_sp_def.pack(side=BOTTOM, pady=10)
 
     # creating a label for the pokemons special attack
     poke_sp_atk = Label(window)
     poke_sp_atk.pack(side=BOTTOM, pady=10)
 
-    # creating a label for the pokemons special defense 
-    poke_sp_def = Label(window)
-    poke_sp_def.pack(side=BOTTOM, pady=10)
-    
-    # creating a label for the pokemons speed
-    poke_speed = Label(window)
-    poke_speed.pack(side=BOTTOM, pady=10)
+    # creating a label for the pokemons defense 
+    poke_def = Label(window)
+    poke_def.pack(side=BOTTOM, pady=10)
+
+    # creating a label for the pokemons attack
+    poke_atk = Label(window)
+    poke_atk.pack(side=BOTTOM, pady=10)
+
+    # creating a label for the pokemons HP
+    poke_hp = Label(window)
+    poke_hp.pack(side=BOTTOM, pady=10)
+
+    # creating a label for the pokemons type
+    poke_type = Label(window)
+    poke_type.pack(side=BOTTOM, pady=10)
+
+    # creating a label for the pokemons pokedex number
+    ndex = Label(window)
+    ndex.pack(side=BOTTOM, pady=10)
+
 
     # selecting a line in the file
     pokemonlst = []  # create a list of pokemon 
@@ -156,19 +158,31 @@ def filescanner(pokemon_image, poke_input, ndex, poke_type, poke_hp, poke_atk, p
 
         # If the user input matches the pokemon name update the data
         if pokemon[0].lower() == user_input.lower():
+
             # creating an empty label field which will display the pokemons image
             png = pokemon[1] + user_input.lower().capitalize() + ".png"
             img = ImageTk.PhotoImage(Image.open(os.path.join(IMAGEROOT, png)).resize((300, 300)))
             pokemon_image.config(image=img)
             pokemon_image.image = img
             ndex.config(text="Pokedex # " + pokemon[1])
-            poke_type.config(text="Type: " + pokemon[2])
+            poke_type.config(text="Primary Type: " + pokemon[2])
             poke_hp.config(text="HP: " + pokemon[3])
             poke_atk.config(text="Attack: " + pokemon[4])
             poke_def.config(text="Defense: " + pokemon[5])
             poke_sp_atk.config(text="Special Attack: " + pokemon[6])
             poke_sp_def.config(text="Special Defense: " + pokemon[7])
             poke_speed.config(text="Speed: " + pokemon[8])
+
+            # changing the font of the stats displayed
+            ndex.config(font=("Simsun", 10))
+            poke_type.config(font=("Simsun", 10))
+            poke_hp.config(font=("Simsun", 10))
+            poke_atk.config(font=("Simsun", 10))
+            poke_def.config(font=("Simsun", 10))
+            poke_sp_atk.config(font=("Simsun", 10))
+            poke_sp_def.config(font=("Simsun", 10))
+            poke_speed.config(font=("Simsun", 10))
+
 
 
 if __name__ == "__main__":
